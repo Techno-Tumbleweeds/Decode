bac8e72ffbc7020fe113d8b89c24a43b7954980b
Techno Tumbleweeds <147280577+Techno-Tumbleweeds@users.noreply.github.com>
2025-09-15 19:49:47 -0600

    Update RobotHardware.java
    Defined drivetrain motors in hardware Set up code structure to make all motors forward and break when not powered

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotHardware.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class RobotHardware {
+    public DcMotor leftFront, rightFront, leftBack, rightBack;
+
+    public void init(HardwareMap hwMap) {
+        // Motor Names and Variables
+        leftFront = hwMap.get(DcMotor.class, "leftFront");
+        rightFront = hwMap.get(DcMotor.class, "rightFront");
+        leftBack = hwMap.get(DcMotor.class, "leftBack");
+        rightBack = hwMap.get(DcMotor.class, "rightBack");
+
+        // Motor Directions
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        
+        // Motor Zero Power Behavior
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
 }
